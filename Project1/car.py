cc622f31bf1b47896373d5af3408c52e726fb760
NoodleMan1404 <64986805+NoodleMan1404@users.noreply.github.com>
2023-09-28 17:59:23 -0700

    Final Commit for project
    FInal commit. There will be bugs, however I don't care to continue to polish the first project of the class.

--- Project1/car.py
+++ Project1/car.py
@@ -15,17 +15,18 @@ class Car:
 
     def mainMenu(self):
         print('\n')
+        print('Main Menu\n')
         print('------------------------------------')
         print(f'Engine Status: {self.engine}')
         print(f'Transmission Status: {self.gear}')
         print('------------------------------------')
         print('What would you like to do?\n\n')
-        choice = input('(T)urn the key, (S)hift gears, or (G)et out of the car, never to return...: ')
+        choice = input('(T)urn the key, (S)hift gears, (D)rive, (I)nfo on your current car, or (G)et out of the car, never to return...: ')
         choice = choice.upper()
         if choice == 'T':
-            if self.engine == True:
+            if self.engine == 'ON':
                 print('You turn the key to the of position, and stop the engine. ')
-                self.engine = False
+                self.engine = 'OFF'
                 exitChoice = input('Would you like to get out of the car? (Y)es / (N)o: ')
                 exitChoice = exitChoice.upper()
                 if exitChoice == 'Y':
@@ -34,9 +35,9 @@ class Car:
                     input('Press any button to continue...')
                     self.mainMenu()
                 self.mainMenu()
-            elif self.engine == False:
+            elif self.engine == 'OFF':
                 print('You turn the key to the on position, and start the engine.')
-                self.engine = True
+                self.engine = 'ON'
                 driveChoice = input('Would you like change gears, and drive? (Y)es / (N)o: ')
                 driveChoice = driveChoice.upper()
                 if driveChoice == 'Y':
@@ -45,32 +46,48 @@ class Car:
                     self.mainMenu()
         if choice == 'S':
             self.trans(self.gear)
+        if choice == 'D':
+            self.drive()
+            if choice == 'I':
+                self.displayInfo()
         if choice == 'G':
             print('You get out, never to return. ')
 
     def drive(self):
-        if self.engine == False:
-            print('The engine is currently off. ')
-        elif self.engine == True:
+        if self.engine == 'OFF':
+            print('The engine is currently off, and can\'t be driven. ')
+            time.sleep(2)
+            self.mainMenu()
+        elif self.engine == 'ON':
             if self.gear == 'D':
-                print("You start driving")
-                time.sleep(3)
-                print('You keep driving')
-                time.sleep(2)
-                keepDChoice = input('Keep driving? (Y)es / (N)o: ')
-                keepDChoice == keepDChoice.upper()
-                if keepDChoice == 'Y':
-                    print('You continue driving forever and never stop')
-                elif keepDChoice == 'N':
-                    print('\n\n')
+                driveChoice = input('Do you want to start driving? (Y)es / (N)o: ')
+                driveChoice = driveChoice.upper()
+                if driveChoice == 'Y':
+                    print("You start driving")
+                    time.sleep(3)
+                    print('You keep driving')
+                    time.sleep(2)
+                    keepDChoice = input('Keep driving? (Y)es / (N)o: ')
+                    keepDChoice = keepDChoice.upper()
+                    if keepDChoice == 'Y':
+                        print('You continue driving forever and never stop')
+                    elif keepDChoice == 'N':
+                        print('\n\n')
+                        print('You stop driving.')
+                        time.sleep(2)
+                        self.mainMenu()
+                elif driveChoice == 'N':
                     self.mainMenu()
             elif self.gear == 'R':
                 print('You start driving backwards, you hit a tree. That\'s it. ')
             elif self.gear == 'N':
                 print('Your engine starts revving because you\'re in neutral. Your engine explodes. That\'s it. ')
+                time.sleep(2)
+                self.mainMenu()
             else:
                 print('Either you selected something that wasn\'t an existing gear, or you\'re in park.')
-                self.drive()
+                time.sleep(2)
+                self.mainMenu()
 
     def setStatus(self, status):
         print(f'The car is currently {status}... ')
@@ -86,15 +103,15 @@ class Car:
         if self.gear == 'P':
             print('You have put the car in Park.\n')
             input('Press any button to continue...')
-            self.mainMenu()
+            self.drive()
         elif self.gear == 'R':
             print('You have put the car in Reverse.\n')
             input('Press any button to continue...')
-            self.mainMenu()
+            self.drive()
         elif self.gear == 'N':
             print('You have put the car in neutral.\n')
             input('Press any button to continue...')
-            self.mainMenu()
+            self.drive()
         elif self.gear == 'D':
             print('You have put the car in Drive.\n')
             input('press any key to continue...')
@@ -103,7 +120,8 @@ class Car:
             print('Only enter a gear to drive. ')
             self.trans(self.gear)
     
-    def displayInfo():
+    def displayInfo(self):
+        print('\n')
         print('---------------------------------------------')
         print('Information about the car you are driving ')
         print('Style', 'Coupe')
@@ -112,7 +130,7 @@ class Car:
         print('Transmission', '8 Speed Automatic')
         print('---------------------------------------------')
 
-my_car = Car(2, 'P', False)
+my_car = Car(2, 'P', 'OFF')
 
 # my_car.drive(my_car.engine, my_car.gear)
 # my_car.trans('P')
